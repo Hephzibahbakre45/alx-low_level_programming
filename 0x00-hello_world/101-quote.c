d7161b2898836afdd314d0fda1a2dfb82a37fc25
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-17 00:36:46 +0100

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main- entry function
+ *
+ * Return: return 1
+ *
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
