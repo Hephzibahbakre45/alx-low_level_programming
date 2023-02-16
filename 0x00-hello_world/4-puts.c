d0865b7c253e61afbfa13575d0c13961a4c62cb7
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-16 22:43:57 +0100

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Description : print a set of string
+ * Return : return 0
+ */
+
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
