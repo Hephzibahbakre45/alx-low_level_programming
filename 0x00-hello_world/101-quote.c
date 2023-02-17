631ef94267981a5dbbcc4f976ffe4a8de0b90d43
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-17 01:57:57 +0100

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,7 @@
 
 int main(void)
 {
-	write(1,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
+	fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n," stdout);
+
 	return (1);
 }
