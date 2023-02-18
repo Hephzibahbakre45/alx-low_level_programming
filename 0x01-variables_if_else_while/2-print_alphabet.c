c2a4565dcb3b0c0e9824c76b9a74847f7ac8d52f
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-18 02:03:50 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Entry level
+ *
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n >= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
