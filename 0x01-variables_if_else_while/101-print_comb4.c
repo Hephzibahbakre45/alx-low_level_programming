61b796f42bddc160e2ad2cf3ca67f8fc6817bec0
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-18 20:29:02 +0100

    task 11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ *
+ * Return: return 0
+ *
+ */
+int main(void)
+{
+	int left_no;
+	int right_no;
+	int center;
+
+	for (left_no = 48; left_no <= 57; left_no++)
+	{
+		for (center = left_no + 1; center <= 57; center++)
+		{
+			for (right_no = center + 1; right_no <= 57; right_no++)
+
+			{
+				putchar(left_no);
+				putchar(center);
+				putchar(right_no);
+
+				if ((left_no == 55) && (center == left_no + 1) && (right_no == center + 1))
+				{
+					break;
+				}
+
+				putchar(',');
+				putchar(' ');
+
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
