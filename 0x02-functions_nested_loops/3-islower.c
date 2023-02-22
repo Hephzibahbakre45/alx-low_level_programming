fef077b3dc9a5cb345a78720245c1fdbf5acde80
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-22 03:42:22 +0100

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - entry point
+ *
+ * @c: contain value to be compared
+ *
+ * Return: 1 if lower, 0 if not
+ *
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122}
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
