390546fa5f8bf32fea182b754b457e8cf18258f0
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-22 03:20:42 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10- print alphabet in lowercase 10x
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char letter;
+
+	while (count++ < = 9)
+	{
+	for (letter = 'a'; letter <= 'z'; letteir++)
+		_putchar(letter);
+
+	_putchar('\n);
+	}
+
+}
