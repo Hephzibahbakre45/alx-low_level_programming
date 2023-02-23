22fa5d3f23cf1278aef11e6d925f7926df4e7973
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-02-23 22:12:58 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul(int a, int b)- entry function
+ * @a: integer
+ * @b: integer
+ * Return: return 0
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
