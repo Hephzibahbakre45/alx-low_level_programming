378ae390386e681df3c4b49e3f3815d0f9529a7d
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-03-23 00:46:43 +0100

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -6,7 +6,6 @@
  * @array: input integer array.
  * @size: size of the array.
  * @action: pointer to the function.
- * 
  * Return: no return.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
@@ -14,6 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 		unsigned int i;
 
 	if (array && action)
-	for (i = 0; i < size; i++
+	for (i = 0; i < size; i++)
 	action(array[i]);
 	}
