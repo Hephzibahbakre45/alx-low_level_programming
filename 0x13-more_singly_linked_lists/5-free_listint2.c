6779628069b6f32c49184a02d85e1f2185651990
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-04-06 02:43:36 +0100

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,10 +13,11 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 	return;
 
-	while(*head == temp)
+	while(*head != NULL)
 	{	
+		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 	}
-	head = NULL;
+
 }
