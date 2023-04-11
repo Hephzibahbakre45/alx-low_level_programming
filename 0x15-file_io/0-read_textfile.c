1c40617f63229f421682e9a75fc693ba11bcc267
Hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-04-11 22:12:26 +0100

    correction to task o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,35 +17,31 @@ size_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_written;
 
 	if (filename == NULL)
-	{
 		return (0);
-	}
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+
+	fd = open(filename, O_RDWR);
+	if (fd < 0)
 	{
 		return (0);
 	}
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(fd);
+		free(fd);
 		return (0);
 	}
 	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
+	if (bytes_read < 1)
 	{
 		free(buffer);
-		close(fd);
 		return (0);
 	}
 	bytes_written = write(STDOUT_FILENO, buffer, byte_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
+	if (bytes_written < 0 || bytes_written != bytes_read)
+
 		return (0);
-	}
-	free(buffer);
+
+
 	close(fd);
 	return (bytes_read);
 
