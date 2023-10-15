a5ccc0a57c20a3490d81ddc539052137dad08564
Hephzibahbakre45 <122830190+Hephzibahbakre45@users.noreply.github.com>
2023-10-16 00:39:09 +0100

    Delete linked_list.c directory

--- linked_list.c/100-first.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
- void before_main() __attribute__((constructor));
-
- void before_main(void)
- {
-    printf("You're beat! and yet, you must allow");
-    printf(",\nI bore my house upon my back!\n");
- }
