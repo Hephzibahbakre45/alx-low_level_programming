021d07169b671dd7acebfd2501ecb03a46e44392
hephzibahbakre45 <ayomidebakre37@gmail.com>
2023-10-16 23:02:25 +0100

    correction of code

--- 0x18-dynamic_libraries/operation.c
+++ 0x18-dynamic_libraries/operation.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
+/**
+ * Main - check the code
+ * add - functiom that add the integer
+ * sub - function that subtract the integer
+ * div - function that divide the integer
+ * mul - function that multiply the integer
+ * @mod - function that modulate the integer
+ * @a - first integer
+ * @b - second integer
+ * Return: Always EXIT_SUCCESS.
+ */
 
-int add (int a, int b)
+int add(int a, int b)
 {
 	return (a + b);
 }
 
-int sub (int a, int b)
+int sub(int a, int b)
 {
 	return (a - b);
 }
 
-int div (int a, int b)
+int div(int a, int b)
 {
 	return (a / b);
 }
 
-int mul (int a, int b)
+int mul(int a, int b)
 {
 	return (a * b);
 }
 
-int mod (int a, int b)
+int mod(int a, int b)
 {
 	return (a % b);
 }
--- 0x18-dynamic_libraries/write.c
+++ 0x18-dynamic_libraries/write.c
@@ -2,11 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * write - function that write the file
+ */
+
+/**
+ * write - function that write the file
+ */
+
 int main(int arg, ...)
 {
 	write(1, "9 8 10 24 75 9\n", 16);
 	write(1, "Please make me win!", 18);
-	
+
 	return (EXIT_SUCCESS);
 
 }
